76ea5a392313c8bd9c67878332764667096bf7cd
connorbturner <connorbryceturner@gmail.com>
2022-09-13 22:08:26 +0200

    Added wiki_graph dataset to the package and added documentation

---
+++ R/wiki_graph.R
@@ -0,0 +1,19 @@
+#' wiki_graph
+#'
+#' A data frame that represents an example graph to use with the dijkstra()
+#' function.
+#'
+#' @format The data frame contains 18 observations of 3 variables that
+#' describe the graph's edges and their weights:
+#'
+#' v1: The starting vector of the edge
+#'
+#' v2: The destination vector of the edge
+#'
+#' w: The weight of the edge
+#'
+#' This example graph is based on the one shown at the top of this wikipedia
+#' article: <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
+#'
+
+"wiki_graph"
